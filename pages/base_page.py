e62ac4b3effdec392f8e5870d436ebf59f8fcdab
denkochin <denkochinsp@gmail.com>
2021-10-20 05:28:15 +0300

    Add several methods as experiment

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,9 +1,25 @@
+from selenium.common.exceptions import NoSuchElementException
+
 class BasePage():
     # конструктор — метод, который вызывается, когда мы создаем объект. 
     # Конструктор объявляется ключевым словом __init__
     def __init__(self, browser, url):
         self.browser = browser
         self.url = url
-    
+
+    # вспомогательный метод поиска элемента в нашей базовой странице 
+    # BasePage, который будет возвращать нам True или False
+    def __init__(self, browser, url, timeout=10):
+        self.browser = browser
+        self.url = url
+        self.browser.implicitly_wait(timeout)
+        
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from .pages.main_page import MainPage
+import pytest
 
 """
 class MainPage():
@@ -27,8 +28,15 @@ def test_add_to_cart(browser):
     page.add_product_to_cart()            # жмем кнопку добавить в корзину 
     page.should_be_success_message()      # проверяем что есть сообщение с нужным текстом
 """
+@pytest.mark.skip
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
